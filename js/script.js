3aa029a05f63850a58f78eeba5d17778f20b06f8
Daniel Raj <raj-dani@hotmail.com>
2020-10-09 08:51:51 +0200

    deleted unused lines

--- js/script.js
+++ js/script.js
@@ -1,10 +1,3 @@
-// MENU TOGGLER
-
-// const selectElement = function (element) {
-//     return document.querySelector(element);
-// };
-
-
 let menuToggler = document.querySelector('.menu-toggle');
 let navLink = document.querySelector('.nav-links');
 let body = document.querySelector('body');
@@ -19,10 +12,3 @@ navLink.addEventListener('click', function(){
     body.classList.toggle('open');
 })
 
-
-// IMAGE GALLERY POP UP
-// let imageGallery = document.querySelector('.image-gallery > a');
-
-// imageGallery.addEventListener('click', function(){
-//     body.classList.toggle('pop-up-view');
-// } )
